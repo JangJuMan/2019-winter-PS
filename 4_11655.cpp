beaa629f293b2feeff1d466f37e7c5b61dfc2aef
JangJuMan <21700633@handong.edu>
2019-12-26 19:09:03 +0900

    Basic Day4 (1/1)

---
+++ 4_11655.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string text;
+    getline(cin, text);
+
+    for(int i=0; i<text.length(); i++){
+        if(isupper(text[i])){
+
+            text[i] = (text[i] + 13 > 90) ? 'A' - 1 + (text[i] + 13) % 90 : text[i] + 13;
+        }
+        else if(islower(text[i])){
+            text[i] = (text[i] + 13 > 122) ? 'a' - 1 + (text[i] + 13) % 122 : text[i] + 13;
+        }
+    }
+
+    cout << text;
+}
