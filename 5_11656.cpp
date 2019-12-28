49f4a7c2a8f1b31ef8c99c3d576f46705f02da7a
JangJuMan <wnaks9901@gmail.com>
2019-12-28 18:02:50 +0900

    day 5

---
+++ 5_11656.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    vector<string> st;
+    string input;
+    cin >> input;
+
+    int len = input.length();
+
+    for(int i=0; i<input.length(); i++){
+        st.push_back(input.substr(i, len--));
+    }
+
+    sort(st.begin(), st.end());
+
+    for(int i=0; i<st.size(); i++){
+        cout << st[i] << "\n";
+    }
+}
