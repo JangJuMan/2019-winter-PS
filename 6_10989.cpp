5b3aac3b9c9c3602b3f367e9f07d4aec14399643
JangJuMan <wnaks9901@gmail.com>
2019-12-30 00:36:15 +0900

    day6 Basic (1/2)

---
+++ 6_10989.cpp
@@ -0,0 +1,24 @@
+// (v)
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n, input;
+    cin >> n;
+    int number[10001];
+
+    memset(number, 0, sizeof(number));
+
+    for(int i=0; i<n; i++){
+        scanf("%d", &input);
+        number[input]++;
+    }
+
+    for(int i=1; i<=10000; i++){
+        for(int j=0; j<number[i]; j++){
+            printf("%d\n", i);
+        }
+    }
+    return 0;
+}
