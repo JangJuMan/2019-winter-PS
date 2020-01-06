d9a8aa027522754500ee0127694c0e4ab3e3b7c7
JangJuMan <wnaks9901@gmail.com>
2020-01-07 02:44:25 +0900

    Day 14 Basic Priority_Q(minHeap)

--- 14_11286.cpp
+++ 14_11286.cpp
@@ -1,7 +1,48 @@
+// (v) using priority_queue
 #include <bits/stdc++.h>
+
+#define din(a) scanf("%d", &a)
+#define chin(a) scanf("%c", &a)
+#define prtD(a) printf("%d\n", a)
 
 using namespace std;
 
 int main(){
-    
+    int n, input, result;
+    din(n);
+    priority_queue<int> minusPQ, plusPQ;
+
+    for(int i=0; i<n; i++){
+        din(input);
+        if(input == 0){
+            if(!plusPQ.empty() && minusPQ.empty()){
+                result = -plusPQ.top();
+                plusPQ.pop();
+            }
+            else if(plusPQ.empty() && !minusPQ.empty()){
+                result = minusPQ.top();
+                minusPQ.pop();
+            }
+            else if(!plusPQ.empty() && !minusPQ.empty()){
+                if(plusPQ.top() > minusPQ.top()){
+                    result = -plusPQ.top();
+                    plusPQ.pop();
+                }
+                else{
+                    result = minusPQ.top();
+                    minusPQ.pop();
+                }
+            }
+            else{
+                result = 0;
+            }
+            prtD(result);
+        }
+        else if(input > 0){
+            plusPQ.push(-input);
+        }
+        else if(input < 0){
+            minusPQ.push(input);
+        }
+    }
 }
